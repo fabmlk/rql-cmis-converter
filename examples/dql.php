fabmlk/rql-cmis-converter

commit 0377ea80651cf043cb87f3e0e81887f5ab360693

Always use alias notation. Add alias resolver to resolve alias dynamically.

examples/dql.php
@@ -1,14 +1,18 @@
 <?php
 
 use Tms\Rql\Factory\DqlFactory;
+use Tms\Rql\ParserExtension\Node\GroupbyNode;
+use Tms\Rql\ParserExtension\Node\Query\FunctionOperator\Dql\AtDepthNode;
+use Xiag\Rql\Parser\AbstractNode;
 use Xiag\Rql\Parser\Lexer;
+use Xiag\Rql\Parser\Node\SelectNode;
+use Xiag\Rql\Parser\Node\SortNode;
 
 require 'dependencies/init.php';
 
 // default lexer supports all RQL rules
 $lexer = new Lexer();
-
-$factory = new DqlFactory('o');
+$factory = new DqlFactory();
 $parser = $factory->getParser();
 
 $rql = '(not(like(document,tot*))|atdepth(1234,4)|eq(identity(group),toto)|(name<>3&d>=string:4&eq(name,-3)))&u!=5&not(or(u=null(),between(i,1,10)))&z=1&(a==2|b<-3|out(c,(2,float:3)))&select(a,sum(b),max(i),identity(a,a))&sort(+a,-b)&limit(1,2)&groupby(type)';

src/Builder/DqlQueryBuilder.php
@@ -26,35 +26,6 @@ use Tms\Rql\ParserExtension\SqlQuery as RqlQuery;
 class DqlQueryBuilder extends SqlQueryBuilder
 {
     /**
-     * @var string
-     */
-    private $rootAlias;
-
-    /**
-     * SqlQueryBuilder constructor.
-     *
-     * @param SqlConditionsBuilder $conditionsBuilder
-     * @param string               $rootAlias the entity alias involved in the construction of the query
-     */
-    public function __construct(SqlConditionsBuilder $conditionsBuilder, string $rootAlias)
-    {
-        parent::__construct($conditionsBuilder);
-        $this->selectQuery = SelectQuery::make($rootAlias);
-        $this->rootAlias = $rootAlias;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function build(RqlQuery $query, string $entity): QueryInterface
-    {
-        $this->process($query);
-        $this->selectQuery->from($entity.' AS '.$this->rootAlias);
-
-        return $this->getQuery();
-    }
-
-    /**
      * Process select node.
      *
      * @param SelectNode|AbstractNode $node
@@ -65,50 +36,22 @@ class DqlQueryBuilder extends SqlQueryBuilder
 
         /** @var SelectNode $node */
         $node = $this->notify($node);
+        $aliases = (array) ($this->aliasResolver)($node);
 
-        foreach ($node->getFields() as $field) {
+        foreach ($node->getFields() as $i => $field) {
+            $alias = $aliases[$i] ?? $aliases[0];
+
             if ($field instanceof AggregateWithValueNode) {
                 if ($field->getValue()) {
-                    $fields[] = e::make(\strtoupper($field->getFunction()).'(%s,%s)', $this->wrapWithRootAlias($field->getField()), $field->getValue());
+                    $fields[] = e::make(\strtoupper($field->getFunction()).'(%s.%s,%s)', $alias, $field->getField(), $field->getValue());
                 } else {
-                    $fields[] = e::make(\strtoupper($field->getFunction()).'(%s)', $this->wrapWithRootAlias($field->getField()));
+                    $fields[] = e::make(\strtoupper($field->getFunction()).'(%s.%s)', $alias, $field->getField());
                 }
             } else {
-                $fields[] = $this->wrapWithRootAlias($field);
+                $fields[] = "$alias.$field";
             }
         }
         $this->selectQuery = $this->selectQuery::make(...$fields);
-    }
-
-    /**
-     * Process sort node.
-     *
-     * @param SortNode|AbstractNode $node
-     */
-    protected function processSortNode(SortNode $node): void
-    {
-        /** @var SortNode $node */
-        $node = $this->notify($node);
-
-        // Convert ['a' => 1, 'b' => -1] to [['o.a', 'ASC'], ['o.b', 'DESC']]
-        $out = [];
-        foreach ($node->getFields() as $field => $direction) {
-            $out[] = [$this->wrapWithRootAlias($field), $direction > 0 ? 'ASC' : 'DESC'];
-        }
-
-        $this->selectQuery->orderBy(...$out);
-    }
-
-    /**
-     * Process group by node.
-     *
-     * @param GroupbyNode|AbstractNode $node
-     */
-    protected function processGroupbyNode(GroupbyNode $node): void
-    {
-        /** @var GroupbyNode $node */
-        $node = $this->notify($node);
-        $this->selectQuery->groupBy(...array_map([$this, 'wrapWithRootAlias'], $node->getFields()));
     }
 
     /**
@@ -117,15 +60,5 @@ class DqlQueryBuilder extends SqlQueryBuilder
     protected function getQuery(): QueryInterface
     {
         return new DqlQuery($this->selectQuery);
-    }
-
-    /**
-     * @param string $field
-     *
-     * @return string
-     */
-    private function wrapWithRootAlias(string $field): string
-    {
-        return $this->rootAlias.'.'.$field;
     }
 }

src/Factory/AliasResolverWrapper.php
@@ -1,0 +1,60 @@
+<?php
+/**
+ * Ce fichier fait partie du package Tms.
+ *
+ * Pour les informations complètes de copyright et de licence,
+ * veuillez vous référer au fichier LICENSE distribué avec ce code source.
+ */
+declare(strict_types=1);
+
+namespace Tms\Rql\Factory;
+
+use Xiag\Rql\Parser\AbstractNode;
+
+/**
+ * Class AliasResolverWrapper.
+ */
+class AliasResolverWrapper
+{
+    /**
+     * @var string
+     */
+    private $defaultAlias;
+
+    /**
+     * @var callable
+     */
+    private $publicAliasResolver;
+
+    /**
+     * AliasResolverWrapper constructor.
+     *
+     * @param callable $publicAliasResolver
+     */
+    public function __construct(callable $publicAliasResolver)
+    {
+        $this->publicAliasResolver = $publicAliasResolver;
+    }
+
+    /**
+     * Utilisé par SqlQueryBuilder après avoir identifié le default alias
+     *
+     * @param string $alias
+     *
+     * @return $this
+     */
+    public function withAlias(string $alias): self
+    {
+        $this->defaultAlias = $alias;
+
+        return $this;
+    }
+
+    /**
+     * @param AbstractNode $node
+     */
+    public function __invoke(AbstractNode $node)
+    {
+        return ($this->publicAliasResolver)($node, $this->defaultAlias);
+    }
+}

examples/cmisql.php
@@ -20,6 +20,6 @@ $builder = $factory->getBuilder(CmisqlFactory::TYPE_SIMPLE);
 $builder->onVisitExpression(new EchoVisitor());
 
 $tree = $parser->parse($tokens);
-$query = $builder->build($tree, 'cmis:document');
+$query = $builder->build($tree, ['cmis:document AS d', 'cmis:folder AS f'], ['inner' => 'd.cmis:objectId = f.cmis:objectId']);
 
 echo SqlFormatter::format($query->sql());

examples/dql-params.php
@@ -8,7 +8,7 @@ require 'dependencies/init.php';
 // default lexer supports all RQL rules
 $lexer = new Lexer();
 
-$factory = new DqlFactory('o');
+$factory = new DqlFactory();
 $parser = $factory->getParser();
 
 $rql = '(not(like(document,tot*))|atdepth(1234,5)|eq(identity(group),toto)|(name<>3&d>=string:4&eq(name,-3)))&u!=5&not(or(u=null(),between(i,1,10)))&z=1&(a==2|b<-3|out(c,(2,float:3)))&select(a,sum(b),max(i),identity(a,a))&sort(+a,-b)&limit(1,2)&groupby(type)';

examples/join.php
@@ -1,0 +1,98 @@
+<?php
+
+use Tms\Rql\Factory\CmisqlFactory;
+use Tms\Rql\ParserExtension\Node\GroupbyNode;
+use Tms\Rql\ParserExtension\Node\Query\FunctionOperator\Cmisql\ContainsNode;
+use Tms\Rql\ParserExtension\Node\Query\FunctionOperator\Cmisql\InFolderNode;
+use Tms\Rql\ParserExtension\Node\Query\FunctionOperator\Cmisql\InTreeNode;
+use Xiag\Rql\Parser\AbstractNode;
+use Xiag\Rql\Parser\Lexer;
+use Xiag\Rql\Parser\Node\SelectNode;
+use Xiag\Rql\Parser\Node\SortNode;
+
+require 'dependencies/init.php';
+
+// default lexer supports all RQL rules
+$lexer = new Lexer();
+
+// arbitrary example of custom alias resolver
+$firstAlias = 'd';
+$secondAlias = 'f';
+$customAliasResolver = function ($node, $rootAlias) use ($secondAlias) {
+    if ($node instanceof SelectNode) {
+        $aliases = [];
+        foreach ($node->getFields() as $i => $field) {
+            if ($field instanceof AbstractNode) {
+                $field = $field->getField();
+                switch ($field) {
+                    case 'i':
+                    case 'b':
+                        $aliases[$i] = $secondAlias;
+                        break;
+                    default:
+                        $aliases[$i] = $rootAlias;
+                }
+            } else {
+                $aliases[$i] = $rootAlias;
+            }
+        }
+
+        return $aliases;
+    }
+    if ($node instanceof SortNode) {
+        $aliases = [];
+        $i = 0;
+        foreach ($node->getFields() as $field => $order) {
+            if ('a' === $field) {
+                $aliases[$i] = $secondAlias;
+            } else {
+                $aliases[$i] = $rootAlias;
+            }
+            $i++;
+        }
+        return $aliases;
+    }
+    if ($node instanceof GroupbyNode) {
+        $aliases = [];
+        foreach ($node->getFields() as $i => $field) {
+            if ('type' === $field) {
+                $aliases[$i] = $secondAlias;
+            } else {
+                $aliases[$i] = $rootAlias;
+            }
+        }
+        return $aliases;
+    }
+
+    if ($node instanceof ContainsNode) {
+        return $rootAlias;
+    }
+    if ($node instanceof InTreeNode || $node instanceof InFolderNode) {
+        return $secondAlias;
+    }
+
+    switch ($node->getField()) {
+        case 'cmis:document':
+        case 'cmis:name':
+        case 'z':
+        case 'd':
+            return $rootAlias;
+        default:
+            return $secondAlias;
+    }
+};
+$factory = new CmisqlFactory($customAliasResolver);
+$parser = $factory->getParser();
+
+$rql = '(not(like(cmis%3Adocument,tot*))|eq(cmis%3Aname,toto)|(cmis%3Aname<>3&d>=string:4&eq(any(cmis%3Aname),-3)))&u!=5&not(or(u=null(),between(i,1,10)))&z=1&contains(coe(l%27hom))&(a==2|b<-3|out(any(c),(2,float:3)))&select(a,sum(b),max(i))&sort(+a,-b)&limit(1,2)&or(intree(e201839d%2Dd3f7%2D4fa1%2Dbbe4%2D7253163ed7a6),infolder(e201839d%2Dd3f7%2D4fa1%2Dbbe4%2D7253163ed7a6))&groupby(type)';
+echo $rql . PHP_EOL;
+
+$tokens = $lexer->tokenize($rql);
+$builder = $factory->getBuilder(CmisqlFactory::TYPE_SIMPLE);
+
+$builder->onVisitExpression(new EchoVisitor());
+
+$tree = $parser->parse($tokens);
+$query = $builder->build($tree, ["cmis:document AS $firstAlias", "cmis:folder AS $secondAlias"], [['inner', "$firstAlias.cmis:objectId = $secondAlias.cmis:objectId"]]);
+
+echo SqlFormatter::format($query->sql());

examples/sql.php
@@ -20,6 +20,6 @@ $builder = $factory->getBuilder(SqlFactory::TYPE_SIMPLE);
 $builder->onVisitExpression(new EchoVisitor());
 
 $tree = $parser->parse($tokens);
-$query = $builder->build($tree, 'my_table');
+$query = $builder->build($tree, 'my_table t');
 
 echo SqlFormatter::format($query->sql());

src/Builder/CmisqlConditionsBuilder.php
@@ -52,6 +52,6 @@ class CmisqlConditionsBuilder extends SqlConditionsBuilder
      */
     protected function getDefaultExpressionVisitor(): callable
     {
-        return new CmisqlSimpleExpressionVisitor();
+        return new CmisqlSimpleExpressionVisitor($this->aliasResolver);
     }
 }

commit a1cf6c4b68516c800332f72a3439c5d4e5e01f15

Fix broken select for DQL caused by 0377ea80651cf043cb87f3e0e81887f5ab360693

examples/dql.php
@@ -15,7 +15,7 @@ $lexer = new Lexer();
 $factory = new DqlFactory();
 $parser = $factory->getParser();
 
-$rql = '(not(like(document,tot*))|atdepth(1234,4)|eq(identity(group),toto)|(name<>3&d>=string:4&eq(name,-3)))&u!=5&not(or(u=null(),between(i,1,10)))&z=1&(a==2|b<-3|out(c,(2,float:3)))&select(a,sum(b),max(i),identity(a,a))&sort(+a,-b)&limit(1,2)&groupby(type)';
+$rql = '(not(like(document,tot*))|atdepth(1234,4)|eq(identity(group),toto)|(name<>3&d>=string:4&eq(name,-3)))&u!=5&not(or(u=null(),between(i,1,10)))&z=1&(a==2|b<-3|out(c,(2,float:3)))&sort(+a,-b)&limit(1,2)&groupby(type)';
 echo $rql . PHP_EOL;
 
 $tokens = $lexer->tokenize($rql);

src/Builder/DqlQueryBuilder.php
@@ -11,6 +11,7 @@ namespace Tms\Rql\Builder;
 
 use Latitude\QueryBuilder\Expression as e;
 use Latitude\QueryBuilder\SelectQuery;
+use Tms\Rql\Factory\AliasResolverWrapper;
 use Tms\Rql\ParserExtension\Node\GroupbyNode;
 use Tms\Rql\ParserExtension\Node\Query\FunctionOperator\Dql\AggregateWithValueNode;
 use Tms\Rql\Query\DqlQuery;
@@ -26,7 +27,18 @@ use Tms\Rql\ParserExtension\SqlQuery as RqlQuery;
 class DqlQueryBuilder extends SqlQueryBuilder
 {
     /**
-     * Process select node.
+     * {@inheritdoc}
+     */
+    protected function process(RqlQuery $query): void
+    {
+        if (null === $query->getSelect()) {
+            $this->selectQuery = $this->selectQuery::make($this->aliasResolver->getDefaultAlias());
+        }
+        parent::process($query);
+    }
+
+    /**
+     * {@inheritdoc}
      *
      * @param SelectNode|AbstractNode $node
      */
@@ -51,6 +63,7 @@ class DqlQueryBuilder extends SqlQueryBuilder
                 $fields[] = "$alias.$field";
             }
         }
+
         $this->selectQuery = $this->selectQuery::make(...$fields);
     }
 

src/Factory/AliasResolverWrapper.php
@@ -37,7 +37,7 @@ class AliasResolverWrapper
     }
 
     /**
-     * Utilisé par SqlQueryBuilder après avoir identifié le default alias
+     * Used by SqlQueryBuilder after having identified the default alias
      *
      * @param string $alias
      *
@@ -48,6 +48,16 @@ class AliasResolverWrapper
         $this->defaultAlias = $alias;
 
         return $this;
+    }
+
+    /**
+     * Used for instance by DqlQueryBuilder
+     *
+     * @return string
+     */
+    public function getDefaultAlias(): string
+    {
+        return $this->defaultAlias;
     }
 
     /**